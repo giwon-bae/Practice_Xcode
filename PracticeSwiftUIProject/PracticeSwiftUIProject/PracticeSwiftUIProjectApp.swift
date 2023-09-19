a3b5146a9f8d764921869e0ad79e03a7fdb2fa8c
giwon-bae <79121621+giwon-bae@users.noreply.github.com>
2023-09-19 05:43:57 -0700

    upload swiftUI project

---
+++ PracticeSwiftUIProject/PracticeSwiftUIProject/PracticeSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftUIProjectApp.swift
+//  PracticeSwiftUIProject
+//
+//  Created by 제로 on 2023/09/19.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
